edb4ecb3a541fe732b0f47dfa1887834efa86502
Giuseppe Varriale <pepe.saoleo@gmail.com>
2023-08-09 14:36:41 -0300

    exemplo turbo frame

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+  # rota GET /turbo_frame_form
+  def turbo_frame_form; end
+
+  # @rota POST /turbo_frame_submit
+  def turbo_frame_submit
+    extracted_word = params[:any][:qualquer_palavra]
+    render :turbo_frame_form, status: :ok,
+                              locals: { qualquer_palavra: extracted_word, comment: 'turbo_frame_submit ok' }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get :customers, to: 'customers#index'
   root to: 'home#index'
+  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
 end
