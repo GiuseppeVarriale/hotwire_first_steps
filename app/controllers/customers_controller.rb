fea0976670f36dc29eb75c483af28eee545c4ae3
Giuseppe Varriale <pepe.saoleo@gmail.com>
2023-08-09 14:39:32 -0300

    exemplo data stream append

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,7 @@
 class CustomersController < ApplicationController
+  def post_something
+    respond_to do |format|
+      format.turbo_stream {}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
   post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
+  post '/customers/post_something' => 'customers#post_something', as: 'post_something'
 end
